548e5e0950ad4ac0f30215a7155572ac1f7ed0b1
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2024-06-01 15:41:02 +0530

    [ADD]: User Information component done!

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import UserInformation from "@/components/UserInformation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex">
-      <h1>LinkedIn 2</h1>
+    <main className="grid">
+      <section>
+        <UserInformation />
+      </section>
     </main>
   );
 }
